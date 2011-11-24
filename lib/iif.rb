1cb22e514456d5b4be06a19d860d455b11e87923
Aria Stewart <aredridel@nbtsc.org>
2011-11-23 19:54:15 -0500

    Make it actually work

--- lib/iif.rb
+++ lib/iif.rb
@@ -3,6 +3,7 @@ require 'enumerator'
 module Finance
 	class IIF
 		def initialize(filename)
+			@headings = {}
 			if block_given?
 				File.open(filename) do |fh|
 					@fh = fh
@@ -15,7 +16,15 @@ module Finance
 
 		def each
 			if block_given?
-				yield null
+				@fh.each_line do |l|
+					f = l.strip.split("\t")
+					type = f.shift
+					if type[0] == ?!
+						@headings[type[1..-1]] = f
+					else
+						yield({:type => type, :data => Hash[*@headings[type].zip(f).flatten]})
+					end
+				end
 			else
 				return new Enumerable::Enumerator(self, :each)
 			end
